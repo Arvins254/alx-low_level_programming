a57a7a2642100af7d5a022ee4a1e46aca490827a
Arvins254 <marvinrvins97@gmail.com>
2023-02-24 02:19:40 +0300

    interger

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,60 +1,34 @@
 #include <stdio.h>
-void prime_num(void);
-long int reduction(long int num, long int prime);
+#include <math.h>
 /**
- * prime_num - prints the largest prime numbers
- * Return: 0
- */
-
-void prime_num(void)
-{
-	long int i;
-	long int i;
-	long int k = 1231952;
-	int count = 0;
-
-	for (i = 2; i < k + i; i++)
-	{
-		for (j = 2; j < k + 1; j++)
-		{
-			/* rintf("Dividing %ld by %ld\n\n", i, j);*/
-			if (i % j == 0)
-			{
-				count++;
-			}
-		}
-		if (!(count > 2))
-		{
-			printf("%ld is a prime number.\n\n", i);
-			k = reduction(k, i);
-		}
-		count = 0;
-	}
-}
-/**
- * reduction - Entry point
- * @num: an interger
- * @prime: prime number
- * Return: remaining value
- */
-long int reduction(long int num, long int prime)
-{
-	long int remainder = num;
-
-	if (num % prime == 0)
-	{
-		remainder = (num / prime);
-		printf("%ld divided by %ld and reduced to %ld\n\n", num, prime);
-		reduction(remainder, prime);
-	}
-	return (remainder);
-}
-/**
- * main - entry point
- * Return:  0.
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	prime_num();
+	long int n;
+	long int max;
+	long int i;
+
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+		max = n;
+	printf("%ld\n", max);
 	return (0);
 }
