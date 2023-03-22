8186a5eb47eb4375427f00558895acc63863f190
Arvins254 <marvinrvins97@gmail.com>
2023-03-22 17:28:02 +0300

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,7 +16,8 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL},
 	};
-	nt i = 0;
+	int i = 0;
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 	return (ops[i].f);
